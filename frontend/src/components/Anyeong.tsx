d2a32140d0be6495dc83ff07a8bd80a08e081fc5
Nick Askam <asknickfilms@gmail.com>
2020-07-22 22:54:49 -0500

    centering table and adding in rows and columns for full score

--- frontend/src/components/Anyeong.tsx
+++ frontend/src/components/Anyeong.tsx
@@ -10,27 +10,59 @@ export class Anyeong extends Component<Props, {}> {
       <div>
         <h3>Anyeong</h3>
         <p>{this.props.foo}</p>
-        <table>
-          <thead>
-            <tr>
-              <th>Team</th>
-              <th>1</th>
-              <th>2</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Away</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>Home</td>
-              <td>1</td>
-              <td>2</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="centered">
+          <table>
+            <thead>
+              <tr>
+                <th>Team</th>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+                <th>6</th>
+                <th>7</th>
+                <th>8</th>
+                <th>9</th>
+                <th>Score</th>
+                <th>Hits</th>
+                <th>Errors</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Away</td>
+                <td>0</td>
+                <td>0</td>
+                <td>3</td>
+                <td>0</td>
+                <td>1</td>
+                <td>0</td>
+                <td>2</td>
+                <td>1</td>
+                <td>0</td>
+                <td>7</td>
+                <td>10</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>Home</td>
+                <td>1</td>
+                <td>2</td>
+                <td>2</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>7</td>
+                <td>0</td>
+                <td>0</td>
+                <td>12</td>
+                <td>16</td>
+                <td>1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
