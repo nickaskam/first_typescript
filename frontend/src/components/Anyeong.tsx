b6af0944565b3f1bce2b9ed1f8b09cdf651d070d
Nick Askam <asknickfilms@gmail.com>
2020-07-22 22:39:24 -0500

    building initial table in Anyeong

--- frontend/src/components/Anyeong.tsx
+++ frontend/src/components/Anyeong.tsx
@@ -10,6 +10,27 @@ export class Anyeong extends Component<Props, {}> {
       <div>
         <h3>Anyeong</h3>
         <p>{this.props.foo}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Team</th>
+              <th>1</th>
+              <th>2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Away</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>Home</td>
+              <td>1</td>
+              <td>2</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
