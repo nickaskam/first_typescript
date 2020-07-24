a6d92f232fe3155817e9283a897e741931fb9aef
Nick Askam <asknickfilms@gmail.com>
2020-07-24 16:37:50 -0500

    adding box score

--- frontend/src/components/Anyeong.tsx
+++ frontend/src/components/Anyeong.tsx
@@ -12,56 +12,72 @@ export class Anyeong extends Component<Props, {}> {
         <p>{this.props.foo}</p>
         <div className="centered">
           <table>
-            <thead>
-              <tr>
-                <th>Team</th>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-                <th>Score</th>
-                <th>Hits</th>
-                <th>Errors</th>
-              </tr>
-            </thead>
             <tbody>
               <tr>
-                <td>Away</td>
-                <td>0</td>
-                <td>0</td>
-                <td>3</td>
-                <td>0</td>
-                <td>1</td>
-                <td>0</td>
-                <td>2</td>
-                <td>1</td>
-                <td>0</td>
-                <td>7</td>
-                <td>10</td>
-                <td>0</td>
-              </tr>
-              <tr>
                 <td>Home</td>
-                <td>1</td>
-                <td>2</td>
-                <td>2</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>7</td>
-                <td>0</td>
-                <td>0</td>
                 <td>12</td>
-                <td>16</td>
-                <td>1</td>
+                <td>vs.</td>
+                <td>7</td>
+                <td>Away</td>
               </tr>
             </tbody>
           </table>
+        </div>
+        <p></p>
+        <div className="centered">
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Team</th>
+                  <th>1</th>
+                  <th>2</th>
+                  <th>3</th>
+                  <th>4</th>
+                  <th>5</th>
+                  <th>6</th>
+                  <th>7</th>
+                  <th>8</th>
+                  <th>9</th>
+                  <th>Score</th>
+                  <th>Hits</th>
+                  <th>Errors</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Away</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>3</td>
+                  <td>0</td>
+                  <td>1</td>
+                  <td>0</td>
+                  <td>2</td>
+                  <td>1</td>
+                  <td>0</td>
+                  <td>7</td>
+                  <td>10</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <td>Home</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>2</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>7</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>12</td>
+                  <td>16</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
